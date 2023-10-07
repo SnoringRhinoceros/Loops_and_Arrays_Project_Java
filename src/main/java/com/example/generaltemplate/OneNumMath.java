cc8095db2a6b0789d6859e7add698f10c447e96d
David <davidshuschool@gmail.com>
2023-10-07 16:40:27 -0500

    Finished OneNumMath
    Changed some of the output files to not have an extra carriage return
    Changed output to have arrays with spaces in between
    Finished 1c and 1d methods

--- src/main/java/com/example/generaltemplate/OneNumMath.java
+++ src/main/java/com/example/generaltemplate/OneNumMath.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class OneNumMath {
 
     /*
+    Method 1a
+
     Precondition:
     User inputs a string of the integer that they want the multiple of
     and another string of how many multiples of that number they want
@@ -16,14 +18,22 @@ public class OneNumMath {
         int numForMultipleInt = Integer.parseInt(numForMultiple);
         if (numForMultipleInt > 0) {
             ArrayList<Integer> result = new ArrayList<>();
-            for (int mutipleNum = 1; mutipleNum <= Integer.parseInt(numWanted); mutipleNum++) {
-                result.add(numForMultipleInt*mutipleNum);
+            for (int multipleNum = 1; multipleNum <= Integer.parseInt(numWanted); multipleNum++) {
+                result.add(numForMultipleInt*multipleNum);
             }
             return result.toString();
         }
         return "No multiples";
     }
 
+    /*
+    Method 1b
+
+    Precondition:
+    User inputs one string that contains an integer
+
+    returns a string of whether that input is even or odd
+     */
     public String numIsEvenOrOdd(String num) {
         int numInt = Integer.parseInt(num);
         if (numInt % 2 == 0) {
@@ -32,11 +42,70 @@ public class OneNumMath {
         return "Odd";
     }
 
+    /*
+    Method 1c
+
+    Precondition:
+    User inputs one string that contains an integer
+
+    returns a string containing an array of all the factorials of that input
+     */
     public String factorialOfNum(String num) {
-        return "";
+        int numInt = Integer.parseInt(num);
+        if (numInt == 0) {
+            return "1";
+        } else if (numInt < -1) {
+            return "Not possible";
+        }
+        int result = numInt;
+        for (int timesToRun=numInt-1; timesToRun>0; timesToRun--) {
+            result *= timesToRun;
+        }
+        return String.valueOf(result);
     }
 
+
+    /*
+    Method 1d
+    Precondition:
+    User inputs a string that contains an integer
+
+    returns a string containing an array of all the prime factors of the input
+     */
     public String primeFactorizationOfNum(String num) {
-        return "";
+        int numInt = Integer.parseInt(num);
+        if (numInt < 2) {
+            return "Not possible";
+        }
+        ArrayList<Integer> factors = new ArrayList<>();
+        factors.add(numInt);
+        while (true) {
+            for (int numToTest : factors) {
+                int factorOfNumToTest = getFactor(numToTest);
+                if (factorOfNumToTest != -1) {
+                    factors.remove((Integer) numToTest);
+                    factors.add(factorOfNumToTest);
+                    factors.add(numToTest/factorOfNumToTest);
+                    break;
+                }
+                if (factors.lastIndexOf(numToTest) == factors.size()-1) {
+                    return factors.toString();
+                }
+            }
+        }
+    }
+
+    private int getFactor(int num) {
+        if (num<2) {
+            return -1;
+        }
+        int testNum = 2;
+        while (testNum<=Math.sqrt(num)) {
+            if (num % testNum == 0) {
+                return testNum;
+            }
+            testNum++;
+        }
+        return -1;
     }
 }
